b3dd0ae865aee3f6f27f3b64c33de8b3946358f5
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-09 22:25:05 +0600

    send data to server and display that data on client side

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,21 @@ function App() {
         "content-type": "application/json",
       },
       body: JSON.stringify(user),
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset();
+      });
+    // fetch("http://localhost:5000/users", {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(user),
+    // });
   };
   return (
     <>
